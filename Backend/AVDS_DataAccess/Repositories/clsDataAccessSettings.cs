9cecf1c924d6f4930d5315ddd23c9633994983e7
ahmedmihemid <147274315+ahmedmihemid@users.noreply.github.com>
2025-11-30 17:10:46 +0300

    Adding Backend project files

---
+++ Backend/AVDS_DataAccess/Repositories/clsDataAccessSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace AVDS_DataAccess.Repositories
+{
+    static class clsDataAccessSettings
+    {
+
+        public static string ConnectionString = "Server=.;Database=AvdsDB;User Id=sa;Password=123456;";
+
+    }
+}
